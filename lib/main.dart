bde419bf822e4c07e57db7f3a0dbc10576973970
Admin <makwanadharmishtha44@gmail.com>
2023-03-02 17:29:20 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bmi_calculator/screens/HomeScreen/provider/HomeProvider.dart';
+import 'package:bmi_calculator/screens/HomeScreen/view/HomePage.dart';
+import 'package:bmi_calculator/utils/SplashPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+// import "package:provider/provider.dart";
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(
+      ChangeNotifierProvider(
+        create: (context) => HomeProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '/' : (context) => SplashPage(),
+            'Home' : (context) => HomeScreen(),
+          },
+        ),
+      )
+  );
+}
